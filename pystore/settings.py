a089f4391c0dbd340e7773e7c98bf8ddf9a7b5af
NisioHelande <nisiohelande@localhost.localdomain>
2021-11-12 16:24:18 -0300

    add toolbar

--- pystore/settings.py
+++ pystore/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #apps de terceiros
+    'debug_toolbar',
+
     #meus apps
     "users.apps.UsersConfig",
     "pages.apps.PagesConfig",
@@ -52,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'pystore.urls'
@@ -136,4 +140,11 @@ AUTH_USER_MODEL = "users.User"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+#django - debug
+
+import socket # noqa
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + '1' for ip in ips]
 
